d13560f35f3e092cc6c262c4a475ae532c70128c
Andy-147 <42777815+Andy-147@users.noreply.github.com>
2018-11-29 12:55:36 -0500

    Test

---
+++ Game List Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> gamenames = new List<string>();
+
+
+            Console.ReadLine();
+        }
+    }
+}
